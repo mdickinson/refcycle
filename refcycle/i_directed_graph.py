3b5eaa4c40fe3d79ae5d3771f50fbd39c876aa36
Mark Dickinson <mdickinson@enthought.com>
2013-12-01 11:32:07 +0000

    PEP8 fix.

--- refcycle/i_directed_graph.py
+++ refcycle/i_directed_graph.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 class cached_property(object):
     def __init__(self, func):
         self._func = func
