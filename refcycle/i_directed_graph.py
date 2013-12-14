1226446d7f1cb4ead521826132c3f5ca698fb366
Mark Dickinson <dickinsm@gmail.com>
2013-12-14 10:34:05 +0000

    Remove unused cached_property class.

--- refcycle/i_directed_graph.py
+++ refcycle/i_directed_graph.py
@@ -18,18 +18,6 @@ Abstract base class for the various flavours of directed graph.
 import abc
 
 
-class cached_property(object):
-    def __init__(self, func):
-        self._func = func
-        self.__name__ = func.__name__
-        self.__doc__ = func.__doc__
-
-    def __get__(self, obj, type):
-        result = self._func(obj)
-        obj.__dict__[self.__name__] = result
-        return result
-
-
 class IDirectedGraph(object):
     """
     Abstract base class for directed graphs.
